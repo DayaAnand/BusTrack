67a0bb99db748d6fd28aa80c2ab0b8e33ce947bb
Tarun Kishore <tarunksu30@gmail.com>
2022-03-10 22:42:05 +0530

    Added splash screen

--- app/src/main/java/com/sentinels/guardian/MainActivity.java
+++ app/src/main/java/com/sentinels/guardian/MainActivity.java
@@ -2,14 +2,33 @@ package com.sentinels.guardian;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        testing webhook for github
+        if(getSupportActionBar()!=null){
+            getSupportActionBar().hide();
+        }
+        Handler handler = new Handler();
+        Runnable r = new Runnable() {
+
+            private Context MainActivity;
+            private Context HomeActivity;
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, com.sentinels.guardian.HomeActivity.class);
+                startActivity(intent);
+            }
+        };
+        handler.postDelayed(r,3000);
     }
 }
