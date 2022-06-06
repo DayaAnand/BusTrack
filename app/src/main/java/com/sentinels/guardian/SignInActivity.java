e0031b13bf55a8b246a0428036d0d2538ce2c60b
Eniyan Maran <74698190+Eniyan27@users.noreply.github.com>
2022-06-06 23:05:46 +0530

    Added UI for Login and Registration Page

--- app/src/main/java/com/sentinels/guardian/SignInActivity.java
+++ app/src/main/java/com/sentinels/guardian/SignInActivity.java
@@ -29,7 +29,7 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_sign);
         // taking instance of FirebaseAuth
         mAuth = FirebaseAuth.getInstance();
 
