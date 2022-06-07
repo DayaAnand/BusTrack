8ca6fef9c868730b03e5882a599834983f436dd2
Eniyan Maran <74698190+Eniyan27@users.noreply.github.com>
2022-06-07 17:40:57 +0530

    Fixed Registration

---
+++ app/src/main/java/com/sentinels/guardian/MainActivity.java
@@ -0,0 +1,30 @@
+package com.sentinels.guardian;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(getApplicationContext(),SignInActivity.class);
+        startActivity(intent);
+        finish();
+        Log.d("Is user ?", String.valueOf(FirebaseAuth.getInstance().getCurrentUser()));
+//        if(FirebaseAuth.getInstance().getCurrentUser()==null){
+//            Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+//            startActivity(intent);
+//            finish();
+//        }
+    }
+}
